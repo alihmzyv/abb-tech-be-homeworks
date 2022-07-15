62cda91fafb14193344b6cc78da970de018fb26a
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-07-15 14:01:21 +0400

    needs a check, +comments

---
+++ homework4/homework5_gitlab/src/main/java/Family.java
@@ -0,0 +1,148 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Family {
+
+    static {
+        System.out.println("Family class is being loaded..");
+    }
+
+    //non-static
+    {
+        System.out.println("A Family type object is created");
+    }
+
+    final private Human mother;
+    final private Human father;
+    private Human[] children;
+    private Pet pet;
+
+
+    //constructor
+    public Family(Human mother, Human father) {
+        /*
+        The only condition for creating a new family is the presence of two parents,
+        with the parents being referred to the current new family and
+        the family being created with an empty array of children.
+         */
+        this.mother = mother;
+        this.father = father;
+
+        //sets the family fields of
+        //both the Human objects passed as parameters to constructor and instance variable parents
+        //to this family object
+        this.mother.setFamily(this);
+        this.father.setFamily(this);
+        children = new Human[0]; //initialized the children array to an empty array
+        //empty array def. from oracle: https://docs.oracle.com/javase/specs/jls/se7/html/jls-10.html
+        /*
+        An array object contains a number of variables.
+        The number of variables may be zero, in which case the array is said to be empty.
+
+         */
+    }
+
+    //getters and setters (none for mother and father, since they should be final
+    public Human getMother() {
+        return mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public Human[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Human[] children) {
+        this.children = children;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+
+    //methods
+    public boolean addChild(Human human) {
+        children = Arrays.copyOf(children, children.length + 1); //increase length by 1
+        children[children.length-1] = human; //add the child
+        children[children.length-1].setFamily(this); //sets the families of both the parameter object and element of field array children
+        return true; //added
+    }
+
+    public boolean deleteChild(int arrIndex) { //removes the child at the following index
+        //create 2 arrays representing children array but the element at given index + 1
+        //then join both arrays and assign it to children array variable
+
+        try {
+            children[arrIndex + 1].setFamily(null); //removes the child's link to the family (+ the link of the object that was added as a child, which is of the importance here)
+            Human[] arr1 = Arrays.copyOfRange(children, 0, arrIndex + 1); //till element at index + 1
+            Human[] arr2 = Arrays.copyOfRange(children, arrIndex + 2, children.length); //after element at index + 1
+            Human[] arr3 = new Human[children.length - 1];
+            System.arraycopy(arr1, 0, arr3, 0, arr1.length);
+            System.arraycopy(arr2, 0, arr3, arrIndex + 1, arr2.length);
+            children = arr3;
+            return true;
+        }
+        catch (ArrayIndexOutOfBoundsException exc) { //only exception that can be thrown
+            System.out.println(exc);
+            return false;
+        }
+    }
+
+    public boolean deleteChild(Human human) {
+        boolean childRemoved = false;
+
+        for (int i = 0; i < children.length; i++) {
+            if(children[i].equals(human)) {
+                childRemoved = deleteChild(i - 1);
+            }
+        }
+
+        return childRemoved;
+    }
+
+    public int countFamily() {
+        return children.length + 2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        //append info about parents
+        sb.append(String.format("Mother=%s\nFather=%s\n", mother.toString(), father.toString()));
+
+        //append info about each children
+        for (int i = 0; i < children.length; i++) {
+            sb.append(String.format("Child %d=%s\n", i, children[i].toString()));
+        }
+
+        //append info about pet
+        sb.append(String.format("Pet=%s\n", pet.toString()));
+
+        return sb.toString();
+    }
+
+    //for equality: all the fields are compared
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return getMother().equals(family.getMother()) && getFather().equals(family.getFather()) && Arrays.deepEquals(getChildren(), family.getChildren()) && Objects.equals(getPet(), family.getPet());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getMother(), getFather(), getPet());
+        result = 31 * result + Arrays.deepHashCode(getChildren());
+        return result;
+    }
+}
---
+++ homework4/homework5_gitlab/src/main/java/Human.java
@@ -0,0 +1,166 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+
+public class Human {
+
+    static {
+        System.out.println("Human class is being loaded..");
+    }
+
+    //non-static
+    {
+        System.out.println("A Human type object is created");
+    }
+
+
+    private String name;
+    private String surname;
+    private Family family;
+    private int year;
+    private int iq;
+    private String[][] schedule;
+
+
+    //constructors
+    //constructor #1
+    public Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+    
+    //constructor #2
+    public Human(String name, String surname, int year, int iq, String[][] schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.schedule = schedule;
+    }
+    
+    //constructor #3
+    public Human(String name, String surname, Family family, int year, int iq, String[][] schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.family = family;
+        this.year = year;
+        this.iq = iq;
+        this.schedule = schedule;
+    }
+
+    //constructor #4
+    public Human() {
+    }
+
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Family getFamily() {
+        return family;
+    }
+
+    public void setFamily(Family family) {
+        this.family = family;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public String[][] getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String[][] schedule) {
+        this.schedule = schedule;
+    }
+
+
+
+    //methods
+    public void greetPet() {
+        System.out.printf("Hello, %s", family.getPet().getNickname());
+        System.out.println();
+    }
+
+    public void describePet() {
+        System.out.printf("I have a %s, he is %d years old, he is %s", family.getPet().getSpecies(), family.getPet().getAge(), family.getPet().getTrickLevel() > 50 ? "very sly" : "almost not sly");
+        System.out.println();
+    }
+
+    public boolean feedPet(boolean itIsTimeForFeeding) {
+        String feed = String.format("Hm... I will feed %s", family.getPet().getNickname());
+        String notFeed = String.format("I think %s is not hungry.", family.getPet().getNickname());
+        boolean wasFed = false;
+
+        if (itIsTimeForFeeding) {
+            System.out.println(feed);
+            wasFed = true;
+        }
+        else {
+            if (family.getPet().getTrickLevel() > new Random().nextInt(101)) {
+                System.out.println(feed);
+                wasFed = true;
+            }
+            else {
+                System.out.println(notFeed);
+            }
+        }
+
+        return wasFed;
+    }
+
+    @Override
+    public String toString() {
+        /*
+        Human{name='Name', surname='Surname', year=1, iq=1, schedule=[[day, task], [day_2, task_2]]}
+         */
+        return String.format("Human{name=%s, surname=%s, year=%d, iq=%d, schedule=%s}",
+                                name, surname, year, iq, Arrays.toString(schedule));
+    }
+
+
+    //for equality: all the fields are compared
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return getYear() == human.getYear() && getIq() == human.getIq() && Objects.equals(getName(), human.getName()) && Objects.equals(getSurname(), human.getSurname()) && Objects.equals(getFamily(), human.getFamily()) && Arrays.deepEquals(getSchedule(), human.getSchedule());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getName(), getSurname(), getFamily(), getYear(), getIq());
+        result = 31 * result + Arrays.deepHashCode(getSchedule());
+        return result;
+    }
+}
