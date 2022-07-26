529fa9e342f8ace4325312a0778eef0905a8a8b2
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-07-26 13:02:20 +0400

    added advanced complexity part

--- homework6/src/main/java/Species.java
+++ homework6/src/main/java/Species.java
@@ -1,7 +1,16 @@
 public enum Species {
-        CAT,
-        DOG,
-        BIRD,
-        FISH
+        CAT(false, 4, true),
+        DOG(false, 4, true),
+        BIRD(true, 2, true),
+        FISH(false, 0, false);
 
+        final boolean canFly; //except canFly, features can be altered
+        int numberOfLegs;
+        boolean hasFur;
+        private
+        Species(boolean canFly, int numberOfLegs, boolean hasFur) {
+                this.canFly = canFly;
+                this.numberOfLegs = numberOfLegs;
+                this.hasFur = hasFur;
+        }
 }
